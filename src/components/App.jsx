064da26000e6d87108705f7f770f8a63e5eb3854
rizorno <114782234+rizorno@users.noreply.github.com>
2023-02-06 09:16:46 +0100

    Updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,7 +23,7 @@ export class App extends Component {
     }
   }
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(_, prevState) {
     if (this.state.contacts !== prevState.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
